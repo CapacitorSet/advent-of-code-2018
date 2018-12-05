22b22034c8cf1f0a83a8a0efe140ba30bf1e2046
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2018-12-05 13:48:59 +0100

    Refactor 3/1

--- 3/1/main.scala
+++ 3/1/main.scala
@@ -2,16 +2,21 @@ import java.awt.Rectangle
 import scala.io.Source
 
 object Application {
+	def rectToPoints(rect: Rectangle) =
+		for {x <- rect.x until rect.x + rect.width
+			 y <- rect.y until rect.y + rect.height}
+			yield (x, y)
+
 	def main(args: Array[String]): Unit = {
 		val format = raw"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)".r()
 		val claims: Iterator[Rectangle] = Source.fromFile("input").getLines
-			.map { case format(id, x, y, dx, dy) => new Rectangle(x.toInt, y.toInt, dx.toInt, dy.toInt) }
+			.map { case format(_, x, y, dx, dy) => new Rectangle(x.toInt, y.toInt, dx.toInt, dy.toInt) }
 		// For each pair of claims,
 		val out = claims.toList.combinations(2)
 			// intersect them
-			.map(pair => pair(0).intersection(pair(1)))
+			.map { case List(left, right) => left intersection right }
 			// get inner points
-			.flatMap(rect => for {x <- rect.x until rect.x + rect.width; y <- rect.y until rect.y + rect.height} yield (x, y))
+			.flatMap(rectToPoints)
 			// count the number of unique points
 			.toSet.size
 		println(out)
