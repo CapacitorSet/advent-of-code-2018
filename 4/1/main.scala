5f550f16bf204691b4b8b199777da4da6cc826a3
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2018-12-05 15:24:49 +0100

    Refactor day 4: skip unused data

--- 4/1/main.scala
+++ 4/1/main.scala
@@ -2,9 +2,9 @@ import scala.io.Source
 
 object Application {
 	def main(args: Array[String]): Unit = {
-		val formatBegin = raw"\[....-(\d\d)-(\d\d) \d\d:\d\d\] Guard #(\d+) begins shift".r()
-		val formatAsleep = raw"\[....-(\d\d)-(\d\d) 00:(\d\d)\] falls asleep".r()
-		val formatWakeup = raw"\[....-(\d\d)-(\d\d) 00:(\d\d)\] wakes up".r()
+		val formatBegin = raw"\[....-\d\d-\d\d \d\d:\d\d\] Guard #(\d+) begins shift".r()
+		val formatAsleep = raw"\[....-\d\d-\d\d 00:(\d\d)\] falls asleep".r()
+		val formatWakeup = raw"\[....-\d\d-\d\d 00:(\d\d)\] wakes up".r()
 		// The events are in random chronological order, but a simple sort will fix that
 		val inputEvents = Source.fromFile("input").getLines.toList.sorted
 		var currentGuardId = 0
@@ -13,21 +13,20 @@ object Application {
 		val WAKEUP = 2
 		val normalizedEvents = for (event <- inputEvents)
 			yield event match {
-				case formatBegin(month, day, guardId) => {
+				case formatBegin(guardId) =>
 					currentGuardId = guardId.toInt
-					(currentGuardId, 32 * month.toInt + day.toInt, 0, BEGIN)
-				}
-				case formatAsleep(month, day, minute) =>
-					(currentGuardId, 32 * month.toInt + day.toInt, minute.toInt, ASLEEP)
-				case formatWakeup(month, day, minute) =>
-					(currentGuardId, 32 * month.toInt + day.toInt, minute.toInt, WAKEUP)
+					(currentGuardId, 0, BEGIN)
+				case formatAsleep(minute) =>
+					(currentGuardId, minute.toInt, ASLEEP)
+				case formatWakeup(minute) =>
+					(currentGuardId, minute.toInt, WAKEUP)
 			}
 		val guardData: Map[Int, Iterator[(Int, Int)]] = normalizedEvents
 			// Map guard IDs to the respective asleep/wakeup events
-			.filterNot(_._4 == BEGIN).groupBy(_._1)
+			.filter(_._3 != BEGIN).groupBy(_._1)
 			// Extract (asleep, wakeup) pairs
     		.mapValues(
-				_.sliding(2, 2).map { case List((_, _, tsAsleep, _), (_, _, tsWakeup, _)) => (tsAsleep, tsWakeup) }
+				_.sliding(2, 2).map { case List((_, tsAsleep, _), (_, tsWakeup, _)) => (tsAsleep, tsWakeup) }
 			)
 		val guardMostAsleep = guardData
 			.mapValues {
