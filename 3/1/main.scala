a62bfe0bbb94c8e21bcee74adfe084df5a8621bc
CapacitorSet <9286933+CapacitorSet@users.noreply.github.com>
2018-12-05 12:32:01 +0100

    Add day 3 part 1

---
+++ 3/1/main.scala
@@ -0,0 +1,19 @@
+import java.awt.Rectangle
+import scala.io.Source
+
+object Application {
+	def main(args: Array[String]): Unit = {
+		val format = raw"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)".r()
+		val claims: Iterator[Rectangle] = Source.fromFile("input").getLines
+			.map { case format(id, x, y, dx, dy) => new Rectangle(x.toInt, y.toInt, dx.toInt, dy.toInt) }
+		// For each pair of claims,
+		val out = claims.toList.combinations(2)
+			// intersect them
+    		.map(pair => pair(0).intersection(pair(1)))
+			// get inner points
+    		.flatMap(rect => for {x <- rect.x until rect.x + rect.width; y <- rect.y until rect.y + rect.height} yield (x, y))
+			// count the number of unique points
+    		.toSet.size
+		println(out)
+	}
+}
